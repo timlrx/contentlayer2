39a870a8ed5e8a65d1753dde75d94e5b514a1ea1
Timothy Lin <timothy0336@hotmail.com>
2024-07-13 13:02:55 +0800

    support only node 18+ and above

--- packages/contentlayer-stackbit-yaml-generator/vitest.config.ts
+++ packages/contentlayer-stackbit-yaml-generator/vitest.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    // If we still need to use the vitest 0.12.10
-    // See README.md for more information why this is needed
-    // threads: false,
     deps: {
       moduleDirectories: ['node_modules', path.resolve('../../packages')],
     },
